830c9f0ea933948c0df06d994844b1b131ff91b8
Bramen <brayan.herrera@utp.edu.co>
2021-05-29 23:28:10 -0500

    Se aplican modelos y librerias correctas para Manejo de usuarios

--- IngSoftware/settings.py
+++ IngSoftware/settings.py
@@ -128,4 +128,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/inicia-sesion/'
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/incia-sesion/'
+
+
+
